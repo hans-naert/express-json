f88d37f208373b4f410bf1833af564d5edf5f7f4
Hans Naert <hans.naert@vives.be>
2023-03-17 20:27:18 +0100

    add post endpoint "append"

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = 3000
 
 app.use(express.static('public'))
 
+app.use(express.json());
+
+app.post('/append', (req, res) => { 
+    console.log(req.body); 
+    res.send(JSON.stringify({result:'ok'}));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- public/script.js
+++ public/script.js
@@ -1,8 +1,20 @@
 console.log("Hello World!");
 
 (async () => {
-let data=await fetch("data.json");
-let data_json=await data.json();
-console.log(data_json);
-document.querySelector('#json_data').textContent=JSON.stringify(data_json);
+    let data = await fetch("data.json");
+    let data_json = await data.json();
+    console.log(data_json);
+    document.querySelector('#json_data').textContent = JSON.stringify(data_json);
+    document.querySelector('#append_button').addEventListener('click', async () => {
+        const data = { username: "example" };
+        let post_fetch= await fetch("/append", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+        let post_response = await post_fetch.json();
+        console.log("Success:", post_response);
+    });
 })();
