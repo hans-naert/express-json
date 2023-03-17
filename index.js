30e54b949bfa07398840c409d7965d9609937ce1
Hans Naert <hans.naert@vives.be>
2023-03-17 20:47:08 +0100

    append data.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
 const port = 3000
 
@@ -8,6 +9,14 @@ app.use(express.json());
 
 app.post('/append', (req, res) => { 
     console.log(req.body); 
+
+    // read file and make object
+    let content = JSON.parse(fs.readFileSync('./public/data.json', 'utf8'));
+    // edit or add property
+    content={...content, ...req.body}
+    //write file
+    fs.writeFileSync('./public/data.json', JSON.stringify(content));
+
     res.send(JSON.stringify({result:'ok'}));
 });
 
